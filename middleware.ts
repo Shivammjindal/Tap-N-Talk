40c572a99ee69c842534503b8533f4fa051c2da6
shivam jindal <shivamjindals2002@gmail.com>
2025-01-21 13:33:26 +0530

    new change

--- middleware.ts
+++ middleware.ts
@@ -7,17 +7,18 @@ export default withAuth({
   }
 });
 
-export const config = {
-  matcher: [
-    "/update/:path*",
-    "/users/:path*",
-    "/conversations/:path*"
-  ]
-};
-
 export function middleware(req: NextRequest){
   console.log("url",req.nextUrl)
   const res = NextResponse.next()
   res.headers.append('ACCESS-CONTROL-ALLOW-ORIGIN','*')
   return res
 } 
+
+export const config = {
+  matcher: [
+    "/api/:path*",
+    "/update/:path*",
+    "/users/:path*",
+    "/conversations/:path*"
+  ]
+};
